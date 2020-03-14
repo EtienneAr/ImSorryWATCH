2b401294e39a4c39add475b9deea294860179fa8
Me <etienne.arlaud@free.fr>
2020-03-14 15:18:28 +0100

    A bit more oversampling

--- firmware/_D_setup_I2C_devices.ino
+++ firmware/_D_setup_I2C_devices.ino
@@ -3,8 +3,8 @@ void setupI2CDevices() {
 	
 	while (!BME280.begin()) { delay(500); }
 
-	BME280.setOversampling(TemperatureSensor,Oversample1);
-	BME280.setOversampling(HumiditySensor,Oversample1);
+	BME280.setOversampling(TemperatureSensor,Oversample4);
+	BME280.setOversampling(HumiditySensor,Oversample4);
 	BME280.setOversampling(PressureSensor,Oversample16);
 
 	BME280.mode(SleepMode);
